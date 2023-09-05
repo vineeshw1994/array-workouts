3aa0de6ea7978d1dc6a5f2a8b6126e58445b1478
vineesh <110625835+vineeshw1994@users.noreply.github.com>
2023-09-05 23:53:25 +0530

    Update A1.js

--- A1.js
+++ A1.js
@@ -1,3 +1,11 @@
 const person =[1,2,3,4,5,6,7,8,9]
 
 console.table(person)
+///////////////////////////
+
+const array = [1,2,3,4,5,6,7,8,9]
+const target = 8
+
+for(let i = 0; i<array.length; i++;){
+  console.log(array[i])
+}
